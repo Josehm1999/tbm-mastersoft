041b40038edc45498ff05e94f0416b8ab89d7f10
JoseHM <josehm1999@gmail.com>
2022-02-23 14:48:37 -0500

    Chore: Changed way of importing functions

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -1,8 +1,8 @@
-import Role from "../models/role.js";
+import Role from '../models/role.js';
 
 const registerRole = async (req, res) => {
   if (!req.body.name || !req.body.description)
-    return res.status(400).send({ message: "Incomplete data" });
+    return res.status(400).send({ message: 'Incomplete data' });
 
   const schema = new Role({
     name: req.body.name,
@@ -12,47 +12,47 @@ const registerRole = async (req, res) => {
 
   const role = await schema.save();
   return !role
-    ? res.status(500).send({ message: "Failed to register role" })
+    ? res.status(500).send({ message: 'Failed to register role' })
     : res.status(200).send({ role });
 };
 
 const roleList = async (req, res) => {
   const roles = await Role.find({
-    name: new RegExp(req.params["name"]),
+    name: new RegExp(req.params['name']),
   });
   return roles.length == 0
-    ? res.status(400).send({ message: "Empty role list" })
+    ? res.status(400).send({ message: 'Empty role list' })
     : res.status(200).send({ roles });
 };
 
 const updateRole = async (req, res) => {
   if (!req.body.description)
-    return res.status(400).send({ message: "Incomplete data" });
+    return res.status(400).send({ message: 'Incomplete data' });
 
   const editedRole = await Role.findByIdAndUpdate(req.body._id, {
     description: req.body.description,
   });
 
   return !editedRole
-    ? res.status(500).send({ message: "Failed to editing role" })
-    : res.status(200).send({ message: "Role updated" });
+    ? res.status(500).send({ message: 'Failed to editing role' })
+    : res.status(200).send({ message: 'Role updated' });
 };
 
 const deleteRole = async (req, res) => {
   const deletedRole = await Role.findByIdAndUpdate(
-    { _id: req.params["_id"] },
+    { _id: req.params['_id'] },
     { dbStatus: false }
   );
   return !deletedRole
-    ? res.status(400).send({ message: "Role no found" })
-    : res.status(200).send({ message: "Role deleted" });
+    ? res.status(400).send({ message: 'Role no found' })
+    : res.status(200).send({ message: 'Role deleted' });
 };
 
 const getRoleById = async (req, res) => {
-  const role = await Role.findById({ _id: req.params["_id"] });
+  const role = await Role.findById({ _id: req.params['_id'] });
   return !role
-    ? res.status(400).send({ message: "No search results" })
+    ? res.status(400).send({ message: 'No search results' })
     : res.status(200).send({ role });
 };
 
-export default { registerRole, roleList, updateRole, deleteRole, getRoleById };
+export { registerRole, roleList, updateRole, deleteRole, getRoleById };
--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,19 +1,20 @@
 import express from 'express';
-import role from '../controllers/role.js';
+import {
+  roleList,
+  getRoleById,
+  updateRole,
+  deleteRole,
+  registerRole,
+} from '../controllers/role.js';
 import roleMidd from '../middlewares/role.js';
 import auth from '../middlewares/auth.js';
 import admin from '../middlewares/admin.js';
 import validId from '../middlewares/validId.js';
 const router = express.Router();
-
-router.post(
-  '/register',
-  [auth, admin, roleMidd.existingRole],
-  role.registerRole
-);
-router.get('/list/:name?', [auth, admin], role.roleList);
-router.get('/find/:_id', [auth, admin, validId], role.getRoleById);
-router.put('/update', [auth, admin, roleMidd.doNotChanges], role.updateRole);
-router.put('/delete/:_id', [auth, admin, validId], role.deleteRole);
+router.post('/register', [auth, admin, roleMidd.existingRole], registerRole);
+router.get('/list/:name?', [auth, admin], roleList);
+router.get('/find/:_id', [auth, admin, validId], getRoleById);
+router.put('/update', [auth, admin, roleMidd.doNotChanges], updateRole);
+router.put('/delete/:_id', [auth, admin, validId], deleteRole);
 
 export default router;
